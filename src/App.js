6e6c30520d8224b8b2a8dcfc05624788fee07075
elemgee <elemgee@gmail.com>
2020-04-11 12:40:30 -0500

    testing success reducer
    Signed-off-by: elemgee <elemgee@gmail.com>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
         <div className="container">
-            <h1>Jotteaux</h1>
+            <h1 className="jumbotron text-center">Jotteaux</h1>
 <Congrats success={true} />
 <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
          </div>
