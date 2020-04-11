152ab4c6631d36e04e242733029337cea3f311d9
elemgee <elemgee@gmail.com>
2020-04-11 18:04:21 -0500

    success tests
    Signed-off-by: elemgee <elemgee@gmail.com>

--- src/GuessInput/GuessInput.test.js
+++ src/GuessInput/GuessInput.test.js
@@ -19,8 +19,8 @@ describe('render', () => {
         });
 
 
-        test('render component without error',() => {
-            const component = findByTestAttr(wrapper,"component-input");
+        test('render component without error', () => {
+            const component = findByTestAttr(wrapper, "component-input");
             expect(component.length).toBe(1);
         });
         test('renders input box', () => {
@@ -35,9 +35,19 @@ describe('render', () => {
 
 
     describe('word has  been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = {success: true};
+            wrapper = setup(initialState);
+        });
+
         test('DOES NOT render input box', () => {
+            const component = findByTestAttr(wrapper, "input-box");
+            expect(component.length).toBe(0);
         });
         test('DOES NOT render submit button', () => {
+            const component = findByTestAttr(wrapper, "submit-button");
+            expect(component.length).toBe(0);
         });
     });
 });
