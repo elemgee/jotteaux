4527d186a7ca4be44df0f9426332036514750c1c
elemgee <elemgee@gmail.com>
2020-04-11 10:17:32 -0500

    adding guessed words
    Signed-off-by: elemgee <elemgee@gmail.com>

---
+++ src/GuessedWords/GuessedWords.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = (props) => {
+    let contents;
+    if (props.guessedWords.length === 0) {
+        contents = (<span data-test="guess-instructions">Try to guess the secret word!</span>)
+    } else {
+        const guessedWordsRows = props.guessedWords.map((guess, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>{guess.guessedWord}</td>
+                <td>{guess.letterMatchCount}></td>
+            </tr>
+        ));
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>guess</th>
+                        <th>matching letters</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {guessedWordsRows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+    return (
+        <div data-test="component-guessed-words">
+            {contents}
+        </div>
+    )
+}
+
+
+GuessedWords.propTypes = {
+    guessedWords: PropTypes.arrayOf(PropTypes.shape({
+        guessedWord: PropTypes.string.isRequired,
+        letterMatchCount: PropTypes.number.isRequired,
+    })).isRequired
+};
+
+export default GuessedWords;
