74111888c69875387429089f6b693d3c35efe703
elemgee <elemgee@gmail.com>
2020-04-15 08:32:04 -0500

    recovered working version of the app without connect problem of input construction
    All tests passing
    
    Signed-off-by: elemgee <elemgee@gmail.com>

--- src/GuessInput/GuessInput.js
+++ src/GuessInput/GuessInput.js
@@ -8,21 +8,23 @@ class GuessInput extends Component {
         const contents = this.props.success
             ? null
             : (
-<form className="form-inline">
-    <input
-    data-test="input-box"
-    type="text"
-    className="mb-2 mx-sm-3"
-    placeholder="enter guess">
-        <button
-            data-test="submit-button"
-            type="submit"
-            className="btn btn-primary mb-2">guess</button>
-</input>
-</form>
+                <form className="form-inline ">
+                    <input
+                        data-test="guess-input"
+                        className="form-control col-10"
+                        id="word-guess"
+                        type="text"
+                        placeholder="enter guess" />
+                        <button
+                            data-test="guess-submit"
+                            type="submit"
+                            className="btn btn-primary ">guess
+                        </button>
+
+                </form>
             );
         return (
-            <div data-test="component-input">
+            <div data-test="component-guess-input" >
                 {contents}
             </div>
         );
@@ -31,7 +33,7 @@ class GuessInput extends Component {
 }
 
 const mapStateToProps = ({success}) => {
-    return { success };
+    return {success};
 }
 
 export default connect(mapStateToProps)(GuessInput);
