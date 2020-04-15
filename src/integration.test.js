d0212e730a5004e622d7575fbcff1bbe2d372b61
elemgee <elemgee@gmail.com>
2020-04-15 09:22:18 -0500

    all tests passing after lecture 72
    Signed-off-by: elemgee <elemgee@gmail.com>

---
+++ src/integration.test.js
@@ -0,0 +1,19 @@
+import {storeFactory} from "../test/TestUtils";
+import {guessWord} from './actions';
+
+
+describe('guessWord action dispatcher',() => {
+
+    describe('no guessed words', () => {
+        test('successful guess updates correctly',() => {});
+        test('unsuccessful guess updates correctly',() => {});
+    });
+
+    describe('some guessed words', () => {
+        test('successful guess updates correctly',() => {});
+        test('unsuccessful guess updates correctly',() => {});
+    });
+
+
+});
+
---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,10 @@
+/**
+ * guessedWordsReducer
+ *
+ * @param state
+ * @param action
+ * @returns {null}
+ */
+export default (state, action) => {
+    return null;
+}
